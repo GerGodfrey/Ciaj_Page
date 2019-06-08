973657bb7fd71beb3a7101ff692c99ed12aba63f
Gerardo <luis-godfrey@hotmail.com>
2019-06-08 11:11:25 -0500

    navbar fine

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 /**BARRA DE NAVEGACION */
 
 let navbar =$(".navbar");
+let navl =$(".nav-link");
+let navi =$(".nav-item");
+
 
 $(window).scroll(function(){
     let oTop = $(".section-2").offset().top - window.innerHeight;
+
     if($(window).scrollTop() > oTop){
         navbar.addClass("sticky");
+        navl.removeClass("nav-link");
+        navi.addClass("nav-item");
     }else{
         navbar.removeClass("sticky");
+        navl.addClass("nav-link");
+        navi.removeClass("nav-item");
     }
 })
