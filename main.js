f2a69049565987e6c4ad4df04689ebc77723c121
Gerardo <luis-godfrey@hotmail.com>
2019-07-01 12:17:49 -0500

    cambios arely 0107

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ $(window).scroll(function(){
         navi.removeClass("nav-item");
     }
 })
+
+var slideIndex = [1, 3];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["carousel1", "carousel2", "carousel3"]
+showSlides(1, 0);
+showSlides(2, 0);
+showSlides(3, 0);
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+}
+function currentSlide(n, no) {
+    showSlides(slideIndex[no] = n, no);
+}
+function showSlides(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) { slideIndex[no] = 1 }
+    if (n < 1) { slideIndex[no] = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[no] - 1].style.display = "block";
+    }
