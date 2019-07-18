e74a364668a3cedc6ca348fdc8cdc060470db41b
Gerardo <luis-godfrey@hotmail.com>
2019-07-17 19:28:58 -0500

    restauración del código

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 /**BARRA DE NAVEGACION */
 
-// let navbar =$(".navbar");
-// let navl =$(".nav-link");
-// let navi =$(".nav-item");
+let navbar =$(".navbar");
+let navl =$(".nav-link");
+let navi =$(".nav-item");
 
 
-// $(window).scroll(function(){
-//     let oTop = $(".section-2").offset().top - window.innerHeight;
+$(window).scroll(function(){
+    let oTop = $(".section-2").offset().top - window.innerHeight;
 
-//     if($(window).scrollTop() > oTop){
-//         navbar.addClass("sticky");
-//         navl.removeClass("nav-link");
-//         navi.addClass("nav-item");
-//     }else{
-//         navbar.removeClass("sticky");
-//         navl.addClass("nav-link");
-//         navi.removeClass("nav-item");
-//     }
-// })
+    if($(window).scrollTop() > oTop){
+        navbar.addClass("sticky");
+        navl.removeClass("nav-link");
+        navi.addClass("nav-item");
+    }else{
+        navbar.removeClass("sticky");
+        navl.addClass("nav-link");
+        navi.removeClass("nav-item");
+    }
+})
 
+
+//PARA QUE NO SE PAUSE EL COROUSEL CUANDO EL MOUSE ESTE ARRIBA
 $('.carousel').carousel({
     pause: "false"
 });
